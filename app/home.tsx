ae6808809c7813d75cc7e36e184eb28b0625e6f4
your_username <benbrill101@gmail.com>
2025-04-30 23:17:00 -0700

    add displaycards

--- app/home.tsx
+++ app/home.tsx
@@ -1,12 +1,16 @@
 import DisplayShows from '@/components/DisplayShows'
-import { Text, View } from 'tamagui'
+import { Text, YStack } from 'tamagui'
 
 export default function Home() {
     return (
-        <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
+        <YStack         
+        flex={1}
+        gap="$2"
+        margin="$3"
+        padding="$2">
         <Text fontFamily="InstrumentSans_400Regular" fontSize={20}>Welcome to</Text>
         <Text fontFamily="InstrumentSerif_400Regular" fontSize={50}>PocketPatron</Text>
         <DisplayShows />
-        </View>
+        </YStack>
     )
 }
--- components/DisplayShows.tsx
+++ components/DisplayShows.tsx
@@ -1,22 +1,58 @@
 import { supabase } from '@/lib/supabase';
-import { View } from 'tamagui';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { Image, ScrollView, Text, View, XStack, YStack } from 'tamagui';
 
+export default function DisplayShows() {
+  const [shows, setShows] = useState<{ show_id: number; title: string; description: string, season: number }[]>([]);
+  const [loading, setLoading] = useState(true);
 
-export default function DisplayShows() { 
+  useEffect(() => {
     async function fetchShows() {
-        const { data: shows, error } = await supabase.from("shows").select('*')
-        if (error) {
-            console.error("Error fetching shows:", error);
-            return null;
-        }
-        console.log("Fetched shows1:", shows);
-        return shows;
+      const { data, error } = await supabase.from('shows').select('*');
+      if (error) {
+        console.error('Error fetching shows:', error);
+      } else {
+        setShows(data || []);
+      }
+      setLoading(false);
     }
-    const shows = fetchShows();
 
+    fetchShows();
+  }, []);
+
+  if (loading) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            
-        </View>
-    )
+      <View f={1} justifyContent="center" alignItems="center">
+        <ActivityIndicator size="large" color="#888" />
+        <Text>Loading shows...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <YStack f={1}>
+      <ScrollView f={1} showsHorizontalScrollIndicator={false}>
+        <YStack px="$4" py="$4">
+        <XStack flexWrap="wrap" justifyContent="space-between">
+          {shows.map((show) => (
+            <YStack key={show.show_id} alignItems='center' mb="$4" width="33%">
+              <Image
+                source={{
+                  uri: `https://vygupxxkyumsyvqotetf.supabase.co/storage/v1/object/public/pocket-patron-covers/covers/2003_Wicked.png`,
+                }}
+                width={100}
+                height={150}
+              />
+              <Text fontSize="$6" fontWeight="bold">
+                {show.title}
+              </Text>
+              <Text>{show.season}</Text>
+            </YStack>
+          ))}
+        </XStack>
+        </YStack>
+      </ScrollView>
+    </YStack>
+  );
 }
